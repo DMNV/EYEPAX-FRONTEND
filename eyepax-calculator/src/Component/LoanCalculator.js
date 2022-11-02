eaf74d4a9724426759ab3072131490da702fe23d
LAPTOP-40MO8B4S\vidun <nadeeramel@gmail.com>
2022-11-02 18:25:01 +0530

    basic opperations

--- eyepax-calculator/src/Component/LoanCalculator.js
+++ eyepax-calculator/src/Component/LoanCalculator.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react'
-import { Button, Col, Form, FormGroup, FormLabel, FormSelect, Row } from 'react-bootstrap'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Col, Form, FormLabel, FormSelect, Row } from 'react-bootstrap'
 import '../loanCal.css'
 
 function LoanCalculator() {
+    const [calculationTypeData, setCalculationTypeData] = useState([]);
     const [monthPaymentData, setMonthPaymentData] = useState('');
-    const [loanAmountData, setLoanAmountData] = useState('');
-    const [interestRateData, setInterestRateData] = useState('');
-    const [noPaymentsData, setNoPaymentsData] = useState('');
+    const [loanAmountData, setLoanAmountData] = useState(10.00);
+    const [interestRateData, setInterestRateData] = useState(10.00);
+    const [noPaymentsData, setNoPaymentsData] = useState(10);
+    const [isSending, setIsSending] = useState(false);
+    useEffect(() => {
+        axios.get(`https://localhost:44357/Calculation`)
+            .then(res => {
+                const data = res.data;
+                setCalculationTypeData(data);
+            })
+    }, [])
 
+
+    const handleSubmit = () => {
+        // don't send again while we are sending
+        if (isSending)
+            return
+
+        setIsSending(true)
+        alert("handleSubmit");
+        setIsSending(false)
+    };
+
+    const handleLoanAmount = (e) => {
+        setLoanAmountData(e.target.value);
+    }
+
+    const handleInterestRate = (e) => {
+        setInterestRateData(e.target.value);
+    }
+
+    const handleNoPayments = (e) => {
+        setNoPaymentsData(e.target.value);
+    }
+
+    const clearAll = () => {
+        setMonthPaymentData('')
+        setLoanAmountData('')
+        setInterestRateData('')
+        setNoPaymentsData('')
+    }
 
     return (
         <div className='CalLayout'>
             <h1 style={{ padding: '0 10px 30px 0' }}>Loan Calculator</h1>
-            <Form>
+            <form>
                 <Row>
                     <Col className='line-padding'>
                         <FormLabel> Calculation Type</FormLabel>
@@ -20,7 +59,9 @@ function LoanCalculator() {
                     <Col xs={8}>
                         <FormSelect aria-label="Default select example">
                             <option>Please select calculation type</option>
-                            <option value="EQUALMONTHLYINSTALLMENT">Equal Monthly Installment (EMI)</option>
+                            {calculationTypeData.map((opt) => (
+                                <option key={opt.id} value={opt.id}>{opt.name}</option>
+                            ))}
                         </FormSelect>
                     </Col>
                 </Row>
@@ -29,7 +70,7 @@ function LoanCalculator() {
                         <FormLabel> Loan Amount (Principle)</FormLabel>
                     </Col>
                     <Col xs={8}>
-                        <Form.Control type="text" value={loanAmountData} />
+                        <Form.Control type="text" value={loanAmountData} onChange={(e) => handleLoanAmount(e)} />
                     </Col>
                 </Row>
                 <Row>
@@ -37,33 +78,29 @@ function LoanCalculator() {
                         <FormLabel>Interest Rate</FormLabel>
                     </Col>
                     <Col xs={8}>
-                        <Form.Control type="text" value={interestRateData} />
+                        <Form.Control type="text" value={interestRateData} onChange={(e) => handleInterestRate(e)} />
                     </Col>
                 </Row>
-                <FormGroup>
-                    <Row>
-                        <Col className='line-padding'>
-                            <FormLabel>No of Payments</FormLabel>
-                        </Col>
-                        <Col xs={8}>
-                            <Form.Control type="text" value={noPaymentsData} />
-                        </Col>
-                    </Row>
-                </FormGroup>
-                <FormGroup>
-                    <Row>
-                        <Col xs={8}></Col>
-                        <Col xs={2} >
-                            <Button variant="primary">Calculate</Button>{' '}
-                        </Col>
-                        <Col xs={2}>
-                            <Button variant="secondary">Clear</Button>{' '}
-                        </Col>
-                    </Row>
-                </FormGroup>
+                <Row>
+                    <Col className='line-padding'>
+                        <FormLabel>No of Payments</FormLabel>
+                    </Col>
+                    <Col xs={8}>
+                        <Form.Control type="text" value={noPaymentsData} onChange={(e) => handleNoPayments(e)} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={8}></Col>
+                    <Col xs={2} >
+                        <button className='btn btn-primary' onClick={() => handleSubmit}>Calculate </button>
+                    </Col>
+                    <Col xs={2}>
+                        <button className='btn btn-secondary' onClick={() => clearAll}>Clear</button>
+                    </Col>
+                </Row>
                 <hr />
                 <FormLabel>Month Payment {monthPaymentData == 0 ? '= $ 0.00' : '= $ ' + monthPaymentData.toFixed(2)}</FormLabel>
-            </Form>
+            </form>
         </div>
     )
 }
